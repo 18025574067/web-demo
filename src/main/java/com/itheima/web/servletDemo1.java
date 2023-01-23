5fc9d583cd73efc003bf6d96bf5bac959cccfdf3
aa <2419805602@qq.com>
2023-01-23 23:50:17 +0800

    home win8 idea

--- src/main/java/com/itheima/web/servletDemo1.java
+++ src/main/java/com/itheima/web/servletDemo1.java
@@ -2,7 +2,10 @@ package com.itheima.web;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet("/demo1")
@@ -11,7 +14,20 @@ public class servletDemo1 implements Servlet {
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("servlet hello world~~");
+        // System.out.println("servlet hello world~~");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String username = request.getParameter("username");
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        if ("zhangsan".equals(username)){
+            writer.write("张三，欢迎您");
+        }else if ("lisi".equals(username)){
+            writer.write("李四，欢迎您");
+        }else {
+            writer.write("您没有权限访问");
+        }
     }
 
     @Override
