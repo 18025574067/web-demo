0447f3647d9d9f81c3953ba96d666520ee10a495
AA <2419805602@qq.com>
2023-01-27 01:53:48 +0800

    home fujitsu win8 idea

---
+++ src/main/java/com/itheima/web/servletDemo2.java
@@ -0,0 +1,36 @@
+package com.itheima.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/demo2")
+public class servletDemo2 implements Servlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("servlet hello world~~");
+    }
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
