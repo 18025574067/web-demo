5468982134ab9f792092c83c36321898755e891c
hyb2419805602 <2419805602@qq.com>
2023-01-27 23:26:05 +0800

    home ubuntu16 idea

--- src/main/java/com/itheima/web/servletDemo2.java
+++ src/main/java/com/itheima/web/servletDemo2.java
@@ -8,16 +8,48 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-@WebServlet("/demo2")
+@WebServlet(urlPatterns = "/demo2", loadOnStartup = 1)
 public class servletDemo2 implements Servlet {
 
+    /**
+     * 初始化方法
+     * 1. 调用时机：默认情况下，Servlet第一次被访问时，会被调用
+     *      * loadOnStartup: -1表示是默认情况，第一次被访问时，才会被调用。
+     *      *               0 或者是 正整数 代表服务器开启就调用了。
+     * 2. 调用次数：1次。
+     * @param servletConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init...");
+    }
+
+    /**
+     *
+     * 提供服务：
+     * 1. 调用时机：每一次servlet被访问时，调用
+     * 2. 调用次数：多次。
+     *
+     *
+     * @param servletRequest
+     * @param servletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("servlet hello world~~");
     }
 
+    /**
+     * 销毁方法：
+     * 1. 调用时机：内存释放或者服务器关闭的时候，Servlet对象会被销毁时调用。
+     * 2. 调用次数：1次
+     */
     @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
+    public void destroy() {
+        System.out.println("destroy...");
     }
 
     @Override
@@ -30,7 +62,5 @@ public class servletDemo2 implements Servlet {
         return null;
     }
 
-    @Override
-    public void destroy() {
-    }
+
 }
