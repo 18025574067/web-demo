a24b9d67c97fbc90dae398523e58b6a7cfc4dff0
hyb2419805602 <2419805602@qq.com>
2023-02-02 22:25:17 +0800

    home ubuntu16 idea

--- src/main/java/com/itheima/web/ServletDemo10.java
+++ src/main/java/com/itheima/web/ServletDemo10.java
@@ -9,13 +9,14 @@ import java.io.IOException;
 
 /**
  * urlPattern:
- * 目录匹配 /user/*
+ * 扩展名匹配 *.do
  */
-@WebServlet(urlPatterns = "/user/*")
-public class ServletDemo9 extends HttpServlet {
+//@WebServlet(urlPatterns = "/*.do")
+@WebServlet(urlPatterns = "*.do")
+public class ServletDemo10 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("demo9 get...");
+        System.out.println("demo10 get...");
     }
 
     @Override
